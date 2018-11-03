b62ffaf1e92f14536380ffeb2697b982808b8800
Phildesro123 <phildesro@yahoo.com>
2018-11-03 16:06:57 -0400

    Fixed routing and ports

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true}).then(()=>{
 });
 
 let routes = require('./routes/route.js');
-console.log(routes);
+
 app.use('/',routes);
-console.log('Express started. Listening on port',process.env.PORT||3000);
-app.listen(process.env.PORT||3000);
+console.log('Express started. Listening on port',process.env.PORT||5000);
+app.listen(process.env.PORT||5000);
