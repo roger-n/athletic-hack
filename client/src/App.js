4759f1fd09556f967d80c4b5060acce4e14911c2
k9isawesome <jzhou381@gatech.edu>
2018-11-03 14:19:42 -0400

    Started Jason branch.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import VersionTable from './components/versionTable';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <VersionTable/>
           <h1 className="App-title">React Express Starter</h1>
         </header>
         <Customers />
