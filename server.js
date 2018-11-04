367e6664876927253f28057d679f7853af4e9a3d
Phildesro123 <phildesro@yahoo.com>
2018-11-03 20:18:58 -0400

    Request error fixed

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 //Connect to MONGODB database
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true}).then(()=>{
  console.log("Connected to MongoDB Server")
