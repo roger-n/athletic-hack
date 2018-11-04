781c264a1cfa8e6518f1793fa3d185c003ec60e1
Roger <39733923+roger-n@users.noreply.github.com>
2018-11-03 20:07:26 -0400

    dfdhsfas

--- client/src/components/UI.jsx
+++ client/src/components/UI.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './UI.css';
 
 
 class UI extends Component {
 
     state = {
-        name: 'newVer',
+        name: '',
         tempArray: [
             {x: 0.5, y: 0.8},
             {x: 0.6, y: 0.3},
@@ -21,32 +22,45 @@ class UI extends Component {
     render () {
         return (
             <div className="UI">
-                <div className="TopPart">
+                <div className="Top-part">
+                    <!--Save button-->
                     <button
                         className="btn btn-primary m-2"
                         onClick={() => (this.props.onSaveClick(this.state.name, this.state.tempArray))}>
                         Submit
                     </button>
+
+                    <!--When field state changes, change name-->
+                    <div className="input-group m-2">
+                        <input
+                            value={this.state.name}
+                            onChange={ this.handleChange }
+                            type="text"
+                            className="form-control"
+                            placeholder="New Player Name"
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"/>
+                    </div>
+                    <!--Testing button click to to function-->
+                    <input
+                        type="button"
+                        value="Alert the text input"
+                        onClick={this.handleClick}
+                    />
                 </div>
                 <div className="CanvasPart">
                     <div>
-                        <input type="text" onChange={ this.handleChange } />
-                        <input
-                            type="button"
-                            value="Alert the text input"
-                            onClick={this.handleClick}
-                        />
+
                     </div>
                 </div>
             </div>
         );
     }
 
-    handleChange = (e)   => {
+    handleChange = (e) => {
         let newState = {...this.state};
-        newState.name = e.target.id;
+        newState.name = e.target.value;
         this.setState(newState);
-        console.log(e.target);
         console.log(this.state.name);
     }
 
